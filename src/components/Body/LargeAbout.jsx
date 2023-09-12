dae03c76c91c95233c487f01d6445948b31b1299
Hailey Hughes <haileyhughes@Haileys-MBP.attlocal.net>
2023-09-12 13:32:12 -0500

    final touches on large screen about

--- src/components/Body/LargeAbout.jsx
+++ src/components/Body/LargeAbout.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import About from "../../Data/Data"
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import About from "../../Data/Data";
 import "../../styles/About.css";
-
-
-
 
 function LargeAbout() {
   return (
     <div className="background-container">
-      
-      <Card className="card">
-      <Card.Body>
+      <Container>
         <Row>
-          <Col sm={8}>
-          {About.text}
-          </Col>
-          <Col sm={4}>
-          <Card.Img src="assets/About.JPG"/>
-          </Col>
+          <h1 className="welcome">{About.welcome}</h1>
         </Row>
-      </Card.Body>
-    </Card>
+        <Card className="card">
+          <Card.Body>
+            <Row className="row">
+              <Col sm={8} className="text">
+                {About.text}
+              </Col>
+              <Col sm={4} className="img">
+                <Card.Img src="assets/About.JPG" />
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
